cce7049f38dd3f284f9d322e2fd71f08b1fe99f9
GaryEstGt <31348611+GaryEstGt@users.noreply.github.com>
2018-11-06 19:47:52 -0600

    interfaz primera parte

---
+++ app/src/main/java/com/example/luise/proyectochat/ChatIndividual.java
@@ -0,0 +1,13 @@
+package com.example.luise.proyectochat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChatIndividual extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_individual);
+    }
+}
